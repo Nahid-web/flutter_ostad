41b4baf0a2bc39c881369e513263c0a6f5a578c0
Nahid-web <nahidamin266@gmail.com>
2023-09-14 23:48:49 +0600

    GridView, SliverGridDelegated

--- lib/body_widget.dart
+++ lib/body_widget.dart
@@ -9,37 +9,9 @@ class BodyWidget extends StatelessWidget {
     return Scrollbar(
       thickness: 10,
       radius: Radius.circular(10),
-      child: ListView(
-
+      child: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4) ,
         children: [
-          ListTile(
-            title: Text('Nafia Amin'),
-            subtitle: Text('info@gmail.com'),
-            leading: Icon(Icons.person_pin),
-            trailing: Icon(Icons.message),
-          ),
-          Divider(
-            color: Colors.pink,
-            thickness: 5,
-            indent: 50,
-          ),
-          ListTile(
-            title: Text('Nafia Amin'),
-            subtitle: Text('info@gmail.com'),
-            leading: Icon(Icons.person_pin),
-            trailing: Icon(Icons.message),
-          ),
-          Divider(
-            color: Colors.black,
-            thickness: 3 ,
-            endIndent: 50,
-          ),
-          ListTile(
-            title: Text('Joy Hadan'),
-            subtitle: Text('info@gmail.com'),
-            leading: Icon(Icons.person_pin),
-            trailing: Icon(Icons.message),
-          ),
           Text('Nahid Amin'),
           Text('Nahid Amin'),
           Text('Nahid Amin'),
