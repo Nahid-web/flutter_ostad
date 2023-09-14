06b7afb72c11730b309016f8fdb0483c157a8b2c
Nahid-web <nahidamin266@gmail.com>
2023-09-14 23:35:13 +0600

    ListView, Divider, ListTile, Scrollbar

--- lib/body_widget.dart
+++ lib/body_widget.dart
@@ -3,17 +3,92 @@ import 'package:flutter_ostad/snack_bar.dart';
 
 class BodyWidget extends StatelessWidget {
   const BodyWidget({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GestureDetector(
-        onTap: () {
-          MySnackBar('this is on tap', context);
-        },
-        onLongPress: (){MySnackBar('Long pressed', context);},
-        onDoubleTap: (){MySnackBar('double tapped', context);},
-        child: Text('this is nahid amin'),
+    return Scrollbar(
+      thickness: 10,
+      radius: Radius.circular(10),
+      child: ListView(
+
+        children: [
+          ListTile(
+            title: Text('Nafia Amin'),
+            subtitle: Text('info@gmail.com'),
+            leading: Icon(Icons.person_pin),
+            trailing: Icon(Icons.message),
+          ),
+          Divider(
+            color: Colors.pink,
+            thickness: 5,
+            indent: 50,
+          ),
+          ListTile(
+            title: Text('Nafia Amin'),
+            subtitle: Text('info@gmail.com'),
+            leading: Icon(Icons.person_pin),
+            trailing: Icon(Icons.message),
+          ),
+          Divider(
+            color: Colors.black,
+            thickness: 3 ,
+            endIndent: 50,
+          ),
+          ListTile(
+            title: Text('Joy Hadan'),
+            subtitle: Text('info@gmail.com'),
+            leading: Icon(Icons.person_pin),
+            trailing: Icon(Icons.message),
+          ),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+          Text('Nahid Amin'),
+        ],
       ),
     );
   }
