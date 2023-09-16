1b8c361cdfe47cd5b5a01e827fa858c104c30a00
Nahid-web <nahidamin266@gmail.com>
2023-09-16 13:19:14 +0600

    navigation push pop

--- lib/body_widget.dart
+++ lib/body_widget.dart
@@ -2,19 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ostad/snack_bar.dart';
 
 class BodyWidget extends StatelessWidget {
-  BodyWidget({super.key});
-
-  List<String> students = ['Nahid', 'Shihab', 'Rifat', 'Shuvo', 'Anas'];
+  const BodyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scrollbar(
-      thickness: 10,
-      radius: Radius.circular(10),
-      child: ListView.separated(
-        itemCount: 5,
-        separatorBuilder: (context, index) => Text(index.toString()),
-        itemBuilder: (context, index) => Text(students[index]),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('This is main page'),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            },
+            child: const Text('Home Screen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SettingScreen()));
+            },
+            child: const Text('Setting Screen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('This is Home Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingScreen()));
+              },
+              child: const Text('Setting Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('back screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('This is Setting Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back Screen'),
+            ),
+          ],
+        ),
       ),
     );
   }
