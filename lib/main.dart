68d58432909dce5045bfdbd517472e6e0189375b
Nahid-web <nahidamin266@gmail.com>
2023-09-18 10:24:08 +0600

    listView.builder done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ostad/material_app_widget.dart';
 
 void main() {
-  runApp(const MaterialAppWidget());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List nameArray = [
+      'Nahid','Shihab','Shimul','Shuvo','Rahim',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: nameArray.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(Icons.person_pin),
+              title: Text(nameArray[index]),
+              subtitle: Text('012343434'),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
