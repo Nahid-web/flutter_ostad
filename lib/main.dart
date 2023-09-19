62ee3ffb6aa074fe7999d8a522c5025b7ff79ad9
Nahid-web <nahidamin266@gmail.com>
2023-09-19 15:41:29 +0600

    TextField

--- lib/main.dart
+++ lib/main.dart
@@ -6,45 +6,72 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('Alear Dialog'),
+          title: const Text('Text Field'),
           centerTitle: true,
         ),
         body: Center(
-          child: button(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const TextField(
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    labelText: 'Your Name',
+                    labelStyle: TextStyle(color: Colors.white70),
+                    hintText: 'Enter your name',
+                    fillColor: Colors.white60,
+                    filled: true,
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const TextField(
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    labelText: 'Your Password',
+                    labelStyle: TextStyle(color: Colors.white70),
+                    hintText: 'Enter your Password',
+                    fillColor: Colors.white60,
+                    filled: true,
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Submit')),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class button extends StatelessWidget{
-  const button({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: (){
-        showDialog(
-            context: context,
-            builder: (BuildContext context){
-              return Expanded(
-                  child: AlertDialog(
-                title: Text('Delte File'),
-                    content: Text('Are u want to delete this file'),
-                    actions: [
-                      TextButton(onPressed: (){}, child: Text('Cancle')),
-                      TextButton(onPressed: (){}, child: Text('Delete')),
-                    ],
-              ));
-            });
-
-      },
-      child: Text('Show Aleart'),
     );
   }
 }
