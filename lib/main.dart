f8f7d82984397c6af75ba5a1e9c1d5aa6ec3b420
Nahid-web <nahidamin266@gmail.com>
2023-09-19 23:41:23 +0600

    Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(const MyApp());
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Home page'),
+          title: const Text('Card'),
           centerTitle: true,
         ),
+
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          NahidPage('Welcome to Nahid Page from Home Page'),
-                    ),
-                  );
-                },
-                child: const Text('Nahid Page'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          NafiaPage('Welcome to Nafia Page from Home Page'),
-                    ),
-                  );
-                },
-                child: const Text('Nafia Page'),
-              ),
-            ],
-          ),
-        ),
+            child: Card(
+              shadowColor: Colors.red,
+                elevation: 20,
+                color: Colors.blueGrey,
+                child: Padding(
+                  padding: const EdgeInsets.all(50),
+                  child: Text('Nahid Amin'),
+                )),),
       ),
     );
   }
 }
 
-class NahidPage extends StatelessWidget {
-  NahidPage(this.message, {super.key});
-  String message;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text(
-          message,
-          style: const TextStyle(fontSize: 12),
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('this is nahid page'),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          NafiaPage('Welcome to Nafia Page from Nahid Page'),
-                    ),
-                  );
-                },
-                child: const Text('Nafia Page')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HomeScreen(),
-                    ),
-                  );
-                },
-                child: const Text('home page')),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class NafiaPage extends StatelessWidget {
-  NafiaPage(this.message, {super.key});
-  String message;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.pink,
-      appBar: AppBar(
-        title: Text(message, style: const TextStyle(fontSize: 12)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('This is Nafia Page'),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          NahidPage('Welcome to Nahid Page from Nafia Page'),
-                    ),
-                  );
-                },
-                child: const Text('Nahid Page')),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const HomeScreen(),
-                    ),
-                  );
-                },
-                child: const Text('home page')),
-          ],
-        ),
-      ),
-    );
-  }
-}
